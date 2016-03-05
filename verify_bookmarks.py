a20d7420c4a02ec5a0c8dba8ef92c21b0371468d
Luca Belluccini <lucabelluccini@gmail.com>
2016-03-05 18:54:57 +0100

    draft

--- verify_bookmarks.py
+++ verify_bookmarks.py
@@ -1,14 +1,17 @@
 import requests
-import re
 from requests.adapters import HTTPAdapter
+
+import re
 from bs4 import BeautifulSoup
+
+from joblib import Parallel, delayed
 
 bookmark_file = """bookmarks_05_03_16.html"""
 
 
 def get_html_tree_from_file(filename):
     with open(filename, 'r') as xml_file:
-        return BeautifulSoup(xml_file, 'lxml')
+        return BeautifulSoup(xml_file, "html.parser")
 
 
 def extract_anchors_from_tree(tree):
@@ -25,6 +28,7 @@ def verify_url(url, request_session):
         status = 'Redirected' if url != r.url else str(r.status_code)
         if status == 'Redirected' and url[4:] == r.url[5:]:
             status = 'HttpsRedirected'
+        r.close()
         return result_build(url, r.url, status)
     except requests.ConnectionError as cex:
         return result_build(url, url, 'ConnectionError: {}'.format(cex.message))
@@ -36,40 +40,28 @@ def verify_url(url, request_session):
         return result_build(url, url, ex.message)
 
 
-def generate_new_bookmark_file(filename, replace_map):
-    new_filename = filename + '.clean'
-    with open(new_filename, 'w') as new_xml_file:
-        with open(filename, 'r') as xml_file:
-            for line in xml_file.readlines():
-                matches = re.match(r'HREF=\"(http.*?)\" ', line)
-                if matches and matches.group(1) in replace_map:
-                    line.replace(matches.group(1), replace_map[matches.group(1)]['redirect_url'])
-                new_xml_file.write(line)
-
-
 def process_anchor(anchor, request_session):
-    print anchor
     result = verify_url(request_session=request_session, url=anchor['href'])
-    print result
-    if result[0] == 'HttpsRedirected':
-        anchor['href'] = result[2]
-    elif result[0] == 'Redirected':
-        anchor['href'] = result[2]
+    if result:
+        if result[0] == 'HttpsRedirected':
+            anchor['href'] = result[2]
+        elif result[0] == 'Redirected':
+            anchor['href'] = result[2]
 
 
 if __name__ == "__main__":
-
     # Prepare a session to set a max retry policy and increase http connections pool size
     session = requests.Session()
-    session.mount('http', HTTPAdapter(max_retries=5, pool_maxsize=100))
+    session.mount('http', HTTPAdapter(max_retries=5, pool_maxsize=16))
 
     # Parse file to obtain html tree
     html_tree = get_html_tree_from_file(bookmark_file)
 
     # Iterate over the anchors
-    for anchor_to_validate in extract_anchors_from_tree(html_tree):
-        process_anchor(anchor_to_validate, session)
+    Parallel(n_jobs=8, backend='threading', verbose=5)(delayed(process_anchor)(anchor_to_validate, session)
+                                                       for anchor_to_validate in extract_anchors_from_tree(html_tree))
 
     # Final file
-    print html_tree.prettify("utf-8")
-
+    output_html = html_tree.prettify("utf-8")
+    with open(bookmark_file + '.clean', 'w') as new_file:
+        new_file.write(output_html)

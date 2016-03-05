be56f3b7a00d9c132b01a462f9b7f112c5e7b09e
Luca Belluccini <lucabelluccini@gmail.com>
2016-03-05 23:09:21 +0100

    Parallel via threads Use Beautifulsoup Generated file can be imported in chrome

--- verify_bookmarks.py
+++ verify_bookmarks.py
@@ -1,12 +1,15 @@
 import requests
 from requests.adapters import HTTPAdapter
-
 import re
 from bs4 import BeautifulSoup
-
 from joblib import Parallel, delayed
+import logging
 
 bookmark_file = """bookmarks_05_03_16.html"""
+logging.basicConfig(filename='trace.log', level=logging.DEBUG)
+logger = logging.getLogger('bookmarks')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.FileHandler(filename='bookmarks.log'))
 
 
 def get_html_tree_from_file(filename):
@@ -25,9 +28,11 @@ def verify_url(url, request_session):
 
     try:
         r = request_session.get(url, allow_redirects=True, timeout=5, stream=True)
-        status = 'Redirected' if url != r.url else str(r.status_code)
-        if status == 'Redirected' and url[4:] == r.url[5:]:
-            status = 'HttpsRedirected'
+        if r.history:
+            for req in r.history:
+                status = req.reason
+        else:
+            status = r.reason
         r.close()
         return result_build(url, r.url, status)
     except requests.ConnectionError as cex:
@@ -43,9 +48,10 @@ def verify_url(url, request_session):
 def process_anchor(anchor, request_session):
     result = verify_url(request_session=request_session, url=anchor['href'])
     if result:
-        if result[0] == 'HttpsRedirected':
+        logger.debug(str(result))
+        if result[0] == 'PermanentRedirect':
             anchor['href'] = result[2]
-        elif result[0] == 'Redirected':
+        elif result[0] == 'Redirect':
             anchor['href'] = result[2]
 
 
@@ -58,10 +64,19 @@ if __name__ == "__main__":
     html_tree = get_html_tree_from_file(bookmark_file)
 
     # Iterate over the anchors
-    Parallel(n_jobs=8, backend='threading', verbose=5)(delayed(process_anchor)(anchor_to_validate, session)
+    Parallel(n_jobs=64, backend='threading', verbose=5)(delayed(process_anchor)(anchor_to_validate, session)
                                                        for anchor_to_validate in extract_anchors_from_tree(html_tree))
 
-    # Final file
-    output_html = html_tree.prettify("utf-8")
-    with open(bookmark_file + '.clean', 'w') as new_file:
-        new_file.write(output_html)
+    # Final file... I know it is not safe but the NETSCAPE-Bookmark-file-1 needs html markup in uppercase...
+    adapted_html = str(html_tree)
+    with open(bookmark_file + '.clean.html', 'w') as new_file:
+        adapted_html = adapted_html.replace('</dt>', '').replace('</p>', '').replace('</meta>', '')
+        adapted_html = adapted_html.replace('h3>', 'H3>')\
+            .replace('</a>', '</A>').replace('<a', '<A').replace('dt>', 'DT>').replace('dl>', 'DL>')\
+            .replace('<h1', '<H1').replace('h1>', 'H1>').replace('<h3', '<H3').replace('<meta', '<META')\
+            .replace('title>', 'TITLE>').replace('add_date=', 'ADD_DATE=').replace('icon=', 'ICON=')\
+            .replace('content=', 'CONTENT=').replace('http-equiv=', 'HTTP-EQUIV=')\
+            .replace('last_modified=', 'LAST_MODIFIED').replace('personal_toolbar_folder=', 'PERSONAL_TOOLBAR_FOLDER=')\
+            .replace('href=', 'HREF=')
+
+        new_file.write(adapted_html)
